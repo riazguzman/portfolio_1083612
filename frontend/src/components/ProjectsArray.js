c80fab606609913a275c19a5e48a50cec70a1053
taegyuy <tgyyun@gmail.com>
2021-09-28 14:52:08 +1000

    dp

--- frontend/src/components/ProjectsArray.js
+++ frontend/src/components/ProjectsArray.js
@@ -10,7 +10,13 @@ const ProjectsArray = () => {
       <Project
         img={unimelbSubjectsPlannerImage}
         name="unimelbSubjectsPlannerImage"
-        description="Unimelb Subjects Planner"
+        description="PERN stack website for University of Melbourne students to plan their course and subjects based on 
+        university guidelines.
+        Built a web scrapper to scrape all subjects listed on the University website and stored this data into a 
+        postgreSQL database.
+        Includes functionality to search and select subjects, and check if those combinations of subjects adhere 
+        to the university guidelines.
+        Frontend built using React, server built using Express.js and Node.js."
       />
       <Project
         img={SnacksInAVanImage}
