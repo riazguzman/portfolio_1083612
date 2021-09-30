a80a60ae7bb45c38acb74da656ebe91c1f735aee
taegyuy <tgyyun@gmail.com>
2021-09-30 15:38:48 +1000

    dp

--- frontend/src/components/Project.js
+++ frontend/src/components/Project.js
@@ -23,7 +23,7 @@ const Project = ({ img, name, description, github, link }) => {
         src={img}
         alt=""
       />
-      <div style={{ marginTop: "20px", marginLeft: "20px" }}>
+      <div style={{ marginLeft: "20px" }}>
         {" "}
         <div style={{ fontWeight: "bold" }}>{name}</div>
         <div style={{ display: "flex", margin: "10px 0" }}>
